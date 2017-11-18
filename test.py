90f4da0950acf2d2b9c8c8f3494a560024c9c391
Polina <okazia@mail.ru>
2017-11-18 18:41:34 +0300

    remove test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-
-data = {'Anna':{'city':'Moskva','temperature':'+2','wind':'east'},'Elena':{'city':'Voronezh','temperature':'+6','wind':'west'},'Vasya':{'city':'Vologda','temperature':'+5','wind':'east'}}
-name = input('Введите имя: ')
-print (data.get(name))
